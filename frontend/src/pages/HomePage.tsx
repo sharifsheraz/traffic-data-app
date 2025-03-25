78f1dd35a29bc19c5417f48807cf70cee58865d1
Sheraz Sharif <sheraz.sharif11@gmail.com>
2025-03-25 20:08:49 +0500

    feat(frontend): hook grouped data api

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
-import { Traffic, VehicleType } from "../types";
 import { BarChart, LineChart, PieChart } from "../components/charts";
 import { COLORS, lang } from "../constants";
 
 export const HomePage = () => {
-  const [data, setData] = useState<Traffic[]>([]);
+  const [data, setData] = useState<{
+    byCountry: { country: string; trafficCount: number }[];
+    byVehicleType: { vehicleType: string; trafficCount: number }[];
+  }>({ byCountry: [], byVehicleType: [] });
+
+  const { byCountry: byCountryData, byVehicleType: byVehicleTypeData } = data;
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/traffic`)
@@ -12,34 +16,6 @@ export const HomePage = () => {
       .then((json) => setData(json))
       .catch((err) => console.error("Error fetching traffic data:", err));
   }, []);
-
-  const countryTraffic = Object.values(
-    data.reduce(
-      (acc, { country, trafficCount }) => {
-        acc[country] = acc[country] || { country, trafficCount: 0 };
-        acc[country].trafficCount += trafficCount;
-        return acc;
-      },
-      {} as Record<string, { country: string; trafficCount: number }>
-    )
-  );
-
-  const vehicleTypeDistribution = Object.values(
-    data.reduce(
-      (acc, { vehicleType, trafficCount }) => {
-        acc[vehicleType] = acc[vehicleType] || {
-          vehicleType,
-          trafficCount: 0,
-        };
-        acc[vehicleType].trafficCount += trafficCount;
-        return acc;
-      },
-      {} as Record<
-        VehicleType,
-        { vehicleType: VehicleType; trafficCount: number }
-      >
-    )
-  );
 
   return (
     <div className="p-2 sm:p-6 space-y-6 max-w-6xl mx-auto">
@@ -49,19 +25,19 @@ export const HomePage = () => {
         </h2>
         <div className="flex flex-col gap-8">
           <BarChart
-            data={countryTraffic}
+            data={byCountryData}
             xKey="country"
             yKey="trafficCount"
             color={COLORS[0]}
           />
           <LineChart
-            data={countryTraffic}
+            data={byCountryData}
             xKey="country"
             yKey="trafficCount"
             color={COLORS[0]}
           />
           <PieChart
-            data={countryTraffic}
+            data={byCountryData}
             dataKey="trafficCount"
             nameKey="country"
             colors={COLORS}
@@ -75,19 +51,19 @@ export const HomePage = () => {
         </h2>
         <div className="flex flex-col gap-8">
           <BarChart
-            data={vehicleTypeDistribution}
+            data={byVehicleTypeData}
             xKey="vehicleType"
             yKey="trafficCount"
             color={COLORS[1]}
           />
           <LineChart
-            data={vehicleTypeDistribution}
+            data={byVehicleTypeData}
             xKey="vehicleType"
             yKey="trafficCount"
             color={COLORS[1]}
           />
           <PieChart
-            data={vehicleTypeDistribution}
+            data={byVehicleTypeData}
             dataKey="trafficCount"
             nameKey="vehicleType"
             colors={COLORS}
