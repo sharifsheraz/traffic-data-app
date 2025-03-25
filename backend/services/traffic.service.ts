97256302c6656e7012dd9b013f5741842f616a1e
Sheraz Sharif <sheraz.sharif11@gmail.com>
2025-03-25 20:22:00 +0500

    feat(backend): refactor get data api fetch grouped data from db

--- backend/services/traffic.service.ts
+++ backend/services/traffic.service.ts
@@ -1,35 +1,22 @@
 import { AppDataSource } from '../data-source';
 import { TrafficEvent } from '../entity';
-import { VehicleType } from '../types';
 
 export const getAllTraffic = async () => {
   const trafficRepository = AppDataSource.getRepository(TrafficEvent);
 
-  const trafficData = await trafficRepository.find();
+  const byCountry = await trafficRepository
+    .createQueryBuilder('traffic')
+    .select('traffic.country', 'country')
+    .addSelect('COUNT(*)::int', 'trafficCount')
+    .groupBy('traffic.country')
+    .getRawMany();
 
-  // Group data by country
-  const byCountry: Record<string, { country: string; trafficCount: number }> = {};
-  for (const event of trafficData) {
-    if (!byCountry[event.country]) {
-      byCountry[event.country] = { country: event.country, trafficCount: 0 };
-    }
-    byCountry[event.country].trafficCount++;
-  }
+  const byVehicleType = await trafficRepository
+    .createQueryBuilder('traffic')
+    .select('traffic.vehicleType', 'vehicleType')
+    .addSelect('COUNT(*)::int', 'trafficCount')
+    .groupBy('traffic.vehicleType')
+    .getRawMany();
 
-  // Group data by vehicle type
-  const byVehicleType: Record<VehicleType, { vehicleType: VehicleType; trafficCount: number }> = {
-    Car: { vehicleType: VehicleType.CAR, trafficCount: 0 },
-    Bus: { vehicleType: VehicleType.BUS, trafficCount: 0 },
-    Truck: { vehicleType: VehicleType.TRUCK, trafficCount: 0 },
-    Bike: { vehicleType: VehicleType.BIKE, trafficCount: 0 },
-    Other: { vehicleType: VehicleType.OTHER, trafficCount: 0 },
-  };
-
-  for (const event of trafficData) {
-    if (byVehicleType[event.vehicleType]) {
-      byVehicleType[event.vehicleType].trafficCount++;
-    }
-  }
-
-  return { byCountry: Object.values(byCountry), byVehicleType: Object.values(byVehicleType) };
+  return { byCountry, byVehicleType };
 };
